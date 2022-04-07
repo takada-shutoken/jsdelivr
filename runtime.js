faeb815ec917970181ae9df5c801fafd171af83a
Alex Lee <73172094+takada-shutoken@users.noreply.github.com>
2022-04-07 08:13:25 +0800

    Update runtime.js

--- runtime.js
+++ runtime.js
@@ -22,7 +22,7 @@
     if(String(snum).length === 1 ){
       snum = "0" + snum;
     }
-    document.getElementById("timeDate").innerHTML = "该博客成立了&nbsp"+dnum+"&nbsp天";
+    document.getElementById("timeDate").innerHTML = "新路的小窝坚持了&nbsp"+dnum+"&nbsp天";
     document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
   }
 
